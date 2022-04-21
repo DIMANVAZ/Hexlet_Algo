a3182adc564b2ab5cf6126280bbb7b7528d4f68e
dimanvaz <dimanvaz04@gmail.com>
2022-04-22 00:10:20 +0300

    dynamic_backpack need to refractor calculating based on prev step

--- dynamic_backpack.js
+++ dynamic_backpack.js
@@ -4,24 +4,36 @@
 // Реализуйте и экспортируйте по умолчанию функцию, которая находит максимальную стоимость рюкзака,
 // используйте динамическое программирование.
 function knapsack(items, capacity){
-    let soByCo = [...items].sort((a,b)=> b.cost-a.cost);
-
+    // let soByCo = [...items].sort((a,b)=> b.cost-a.cost);
     let table = [];
-    table.push(['вещь \\ вместимость']);
-    for (let i = 1; i <= capacity; i++) {
-        table[0].push(i);
+    for (let i = 0; i < items.length; i++) {
+        table.push(new Array(capacity).fill(items[i].name));
     }
 
-    for (let y = 0; y < soByCo.length; y++) { // каждый элемент массива items 0-1-2-3
-        let row = new Array(capacity); // пустой массив для строки вещи
-        row[0] = soByCo[y].name; // вбиваем туда имя
-        for (let x = 1; x <= capacity; x++) { // гоним вдоль вместимости рюкзака 1-2-3-...-9
+    // i - элементы входного массива (вещи) -> т.е. высота таблицы
+    // j - capacity рюкзака, от 1 до n -> горизонталь
+    // текущий элемент по горизонтали - это предыдущий максимум + свободный слот, заполненный по возможности
 
+    for (let j = 1; j <= capacity; j++){
+        for (let i = 0; i < items.length; i++) {
+            if(j >= items[i].weight){
+                table[i][j] = items[i].cost;
+            } else {
+                table[i][j] = 0
+            }
         }
-        table.push(row);
     }
-    console.table(table);
+    console.log(table);
+    console.table(table)
 }
+
+const myItems = [
+    { name: 'Кольцо', weight: 1, cost: 50 },
+    { name: 'Зажигалка', weight: 2, cost: 30 },
+    { name: 'Ключ', weight: 3, cost: 20 },
+    { name: 'Болт', weight: 2, cost: 10 },
+]
+
 
 const items = [
     { name: 'porridge', weight: 6, cost: 30 },
@@ -31,9 +43,12 @@ const items = [
 ];
 
 const items2 = [
-    { name: 'backpack', weight: 60, cost: 300 },
-    { name: 'headphones', weight: 10, cost: 200 },
-    { name: 'phone', weight: 30, cost: 150 },
-    { name: 'book', weight: 40, cost: 200 },
+    { name: 'backpack', weight: 6, cost: 30 },
+    { name: 'headphones', weight: 1, cost: 20 },
+    { name: 'book', weight: 4, cost: 20 },
+    { name: 'phone', weight: 3, cost: 15 },
 ];
-knapsack(items, 9); // 55
+
+//knapsack(items, 9); // 55
+//knapsack(items2, 6); // 40
+knapsack(myItems, 6); // 40
