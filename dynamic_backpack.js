41f0b2e6a4469b3fa7f308807ad6bc04bbb1c372
dimanvaz <dimanvaz04@gmail.com>
2022-04-23 14:22:07 +0300

    dynamic_backpack - feat correct code

--- dynamic_backpack.js
+++ dynamic_backpack.js
@@ -4,28 +4,27 @@
 // Реализуйте и экспортируйте по умолчанию функцию, которая находит максимальную стоимость рюкзака,
 // используйте динамическое программирование.
 function knapsack(items, capacity){
-    // let soByCo = [...items].sort((a,b)=> b.cost-a.cost);
-    let table = [];
-    for (let i = 0; i < items.length; i++) {
-        table.push(new Array(capacity).fill(items[i].name));
-    }
+    const n = items.length;
+    const dp = new Array(n + 1).fill(0).map(() => new Array(capacity + 1).fill(0));
 
-    // i - элементы входного массива (вещи) -> т.е. высота таблицы
-    // j - capacity рюкзака, от 1 до n -> горизонталь
-    // текущий элемент по горизонтали - это предыдущий максимум + свободный слот, заполненный по возможности
+    // делает массив из n+1 массивов, каждый из которых длиной capacity+1
+    // получается матрица n+1 по высоте, capacity+1 по ширине
+    // нулевой столбец делают - как я понял - просто чтобы не заморачиваться, т.к. считать вместимость всё равно с 1
 
-    for (let j = 1; j <= capacity; j++){
-        for (let i = 0; i < items.length; i++) {
-            if(j >= items[i].weight){
-                table[i][j] = items[i].cost;
+    for (let i = 1; i <= n; i++) {
+        for (let w = 1; w <= capacity; w++) {
+            if (items[i - 1].weight <= w) {
+                dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - items[i - 1].weight] + items[i - 1].cost);
             } else {
-                table[i][j] = 0
+                dp[i][w] = dp[i - 1][w];
             }
         }
     }
-    console.log(table);
-    console.table(table)
+    console.table(dp);
+    return dp[n][capacity];
 }
+
+
 
 const myItems = [
     { name: 'Кольцо', weight: 1, cost: 50 },
@@ -33,7 +32,6 @@ const myItems = [
     { name: 'Ключ', weight: 3, cost: 20 },
     { name: 'Болт', weight: 2, cost: 10 },
 ]
-
 
 const items = [
     { name: 'porridge', weight: 6, cost: 30 },
@@ -49,6 +47,6 @@ const items2 = [
     { name: 'phone', weight: 3, cost: 15 },
 ];
 
-//knapsack(items, 9); // 55
-//knapsack(items2, 6); // 40
-knapsack(myItems, 6); // 40
+console.log(knapsack(myItems, 6));
+console.log(knapsack(items, 9));
+console.log(knapsack(items2, 6));
