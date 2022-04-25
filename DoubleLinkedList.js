e28a5ffda0f297eecccfdb4902d614de006ed4e9
dimanvaz <dimanvaz04@gmail.com>
2022-04-26 00:26:40 +0300

    feat 3 of 4 raw methods in DLL

--- DoubleLinkedList.js
+++ DoubleLinkedList.js
@@ -19,54 +19,43 @@ class DoubleLinkedList {
             return null;
         }
         let removableElem = null;
-
-        while(this.head && this.head.value === value){ //если искомые идут сразу с начала подряд
-            removableElem = this.head;
-            this.head = this.head.next;
+        // прикол в движении в обе стороны - в next и в previous
+        if(!this.head.next && this.head.value !== value){
+            // просто не меняем нулл
         }
 
-        let thisEl = this.head; // т.к. голова явно уже не подходит, то стартуем с неё
-
-        while(thisEl){ // пока существует текущий элемент
-            if(!thisEl.next){ // след элемент - не существует. Искать нечего
-                return removableElem; // вместо НУЛЛА - см строку 23
-            } else {
-                // 1) следующий элемент - существует
-                if(thisEl.next.value === value){ // 1а)-его значение подходит
-                    removableElem = thisEl.next;
-
-                    if(thisEl.next === this.tail){ // если след элемент - хвост, то
-                        this.tail = thisEl; //... то назначаем хвост данным элементом
-                    } else {
-                        thisEl.next = thisEl.next.next;
-                    }
-                } else { // 1б)-следующий элемент - существует, но его значение не подходит
-                    thisEl = thisEl.next;
-                }
-            }
-        }
         return removableElem;
     }
 
     find(value){ //– находит первый элемент, равный value
         // перебрать весь список, найти первый
-        if(!this.head){
-            return null;
-        }
+        let found = null;
+
         if(this.head.value === value){
-            return this.head;
+            found = this.head;
         }
         if(this.tail.value === value){
-            return this.tail;
+            found = this.tail;
         }
-        let nextEl = this.head.next;
 
-        while(nextEl?.next){
-            if(nextEl.value !== value){
+        let nextEl = this.head.next;
+        let prevEl = this.tail.previous;
+        while(nextEl.next || prevEl.previous){
+            if(nextEl.value === value){
+                found = nextEl;
+                break;
+            }
+            else if(prevEl.value === value){
+                found = prevEl;
+                break;
+            }
+            else {
                 nextEl = nextEl.next;
-            } else return nextEl;
+                prevEl = prevEl.previous;
+            }
         }
-        return null;
+
+        return found;
     }
 
     append(value){ //– добавляет элемент в конец списка
@@ -100,5 +89,5 @@ class DoubleLinkedList {
 
 const doll = new DoubleLinkedList();
 doll.append(7).append(8).append(12).prepend(6).append(15);
-console.log(doll.head);
-console.log(doll.tail.value);
+
+console.log(doll.find(7));
