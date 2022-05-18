46af5b49948ffc72ba64dbc2c2a14e485a6a8d9f
dimanvaz <dimanvaz04@gmail.com>
2022-05-18 20:53:26 +0300

    feat raw list.js

--- sequences/list.js
+++ sequences/list.js
@@ -5,19 +5,49 @@ import { l, cons as consList, head, tail, isEmpty, toString as listToString } fr
 Реализуйте и экспортируйте функцию reverse(), которая переворачивает список, используя итеративный процесс.
 Реализуйте и экспортируйте функцию concat(), которая соединяет два списка, используя рекурсивный процесс
 (попробуйте сначала представить, как работала бы функция copy(), которая принимает на вход список и возвращает его копию).
+
+у списка из 1 элемента нет хвоста! null Но голова есть!
 */
 
-export function has(list, value){}
-export function reverse(list){}
-export function concat(list1, list2){}
+export function has(list, value){
+    if(isEmpty(list)){
+        return false;
+    }
+    if(head(list) === value){
+        return true;
+    }
+    return has(tail(list),value)
+}
+export function reverse(list){
+    if(isEmpty(list)){
+        return list;
+    }
+    let reversed = l();
+    function next(tempList){
+        reversed = consList(head(tempList),reversed);
+        if(!tail(tempList)){
+            return reversed;
+        }
+        return next(tail(tempList))
+    }
+    return next(list);
+}
+
+export function concat(list1, list2){
+
+}
+
+let emp = l(3,4,5,6);
+console.log(listToString(reverse(emp)));
 
 const numbers = l(3, 4, 5, 8);
-has(numbers, 8); // true
-has(numbers, 0); // false
+console.log(has(numbers, 8)); // true
+console.log(has(numbers, 0)); // false
 
 const numbers2 = l(3, 4, 5);
-reverse(numbers2); // (5, 4, 3)
+console.log(listToString(reverse(numbers2))); // (5, 4, 3)
 
 const numbers3 = l(3, 2, 9);
-concat(numbers, numbers3); // (3, 4, 5, 8, 3, 2, 9)
+console.log(listToString(concat(numbers, numbers3))); // (3, 4, 5, 8, 3, 2, 9)
+
 
