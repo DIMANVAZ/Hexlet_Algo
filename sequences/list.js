0a298cd484d1e184d27576158dfe09a2b1d55884
dimanvaz <dimanvaz04@gmail.com>
2022-05-19 13:47:50 +0300

    finished list.js; feat tutor's solution into it

--- sequences/list.js
+++ sequences/list.js
@@ -1,14 +1,37 @@
 import { l, cons as consList, head, tail, isEmpty, toString as listToString } from '@hexlet/pairs-data';
 
-/*
-Реализуйте и экспортируйте функцию has(), которая проверяет, является ли переданное значение элементом списка.
+/* Реализуйте и экспортируйте функцию has(), которая проверяет, является ли переданное значение элементом списка.
 Реализуйте и экспортируйте функцию reverse(), которая переворачивает список, используя итеративный процесс.
 Реализуйте и экспортируйте функцию concat(), которая соединяет два списка, используя рекурсивный процесс
 (попробуйте сначала представить, как работала бы функция copy(), которая принимает на вход список и возвращает его копию).
 
-у списка из 1 элемента нет хвоста! null Но голова есть!
-*/
+У списка из 1 элемента нет хвоста! null Но голова есть!
+consList ставит переданный элемент в начало переданного списка, как ни крути! */
 
+export function copy(listToCopy){
+    return consList(head(listToCopy),tail(listToCopy));
+}
+
+//-----------решение Учителя--------
+const numbers = l(1, 2, 3, 4);
+const numbers2 = l(5, 6, 7);
+
+export const concat = (list1, list2) => {
+    if (isEmpty(list1)) {
+        return list2;
+    }
+    return consList(head(list1), concat(tail(list1), list2));
+};
+
+export const reverse = (list) => {
+    const iter = (items, acc) => (
+        isEmpty(items) ? acc : iter(tail(items), consList(head(items), acc))
+    );
+
+    return iter(list, l());
+};
+
+//-------------мои решения----------------------
 export function has(list, value){
     if(isEmpty(list)){
         return false;
@@ -18,8 +41,7 @@ export function has(list, value){
     }
     return has(tail(list),value)
 }
-
-export function reverse(list){
+export function reverseMy(list){
     if(isEmpty(list)){
         return list;
     }
@@ -33,21 +55,41 @@ export function reverse(list){
     }
     return next(list);
 }
-
-export function concat(list1, list2){
-
+export function concatMy(list1, list2){
+    if(isEmpty(list1)){
+        return list2;
+    }
+    if(isEmpty(list2)){
+        return list1;
+    }
+    let rev = reverse(list1); // разворачиваем первый список
+    let updatedList = consList(head(rev),list2); // начнём сразу с прилепления ко второму списку хвоста первого (головы перевёрнутого)
+    function gluer(listToTake, updatedList){ // принимает след элемент и удлинённый список
+        if(!tail(listToTake)){
+            return updatedList;
+        }
+        updatedList = consList(head(tail(listToTake)),updatedList); // перезаписываем обновлённый список-
+        return gluer(tail(listToTake),updatedList)
+    }
+    return gluer(rev,updatedList);
 }
 
-const numbers = l(1, 2, 3, 4);
-const numbers3 = l(5, 6, 7);
-console.log(listToString(concat(numbers, numbers3))); // (1, 2, 3, 4, 5, 6, 7)
+const list1 = l(1,2);
+console.log(listToString(consList(0,list1)));
+console.log(listToString(list1));
+copy(list1);
 
-let emp = l(3,4,5,6);
-console.log(listToString(tail(emp)))
-console.log(listToString(reverse(emp)));
+console.log(listToString(concat(numbers, numbers2)));//'(3, 4, 5, 8, 3, 2, 9)
+//console.log(listToString(concat(numbers, l())));//(3, 4, 5, 8)
+//console.log(listToString(concat(l(), numbers2)));//(3, 2, 9)
+//console.log(listToString(concat(l(1, 7, 8, 13, 5, 17, 22, 99, 53, 19), numbers2)));//(1, 7, 8, 13, 5, 17, 22, 99, 53, 19, 3, 2, 9)
 
-const numbers2 = l(3, 4, 5);
-console.log(listToString(reverse(numbers2))); // (5, 4, 3)
+// let emp = l(3,4,5,6);
+// console.log(listToString(tail(emp)))
+// console.log(listToString(reverse(emp)));
+//
+// const numbers2 = l(3, 4, 5);
+// console.log(listToString(reverse(numbers2))); // (5, 4, 3)
 
 
 
