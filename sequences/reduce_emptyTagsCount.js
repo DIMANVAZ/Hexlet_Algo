98a59661f685adae65f1df383a0693a648fcb60e
dimanvaz <dimanvaz04@gmail.com>
2022-05-23 17:23:25 +0300

    fix reduce_emptyTagsCount.js

--- sequences/reduce_emptyTagsCount.js
+++ sequences/reduce_emptyTagsCount.js
@@ -6,21 +6,61 @@ import { l, isEmpty, head, tail, cons as consList, reverse, toString as listToSt
 Реализуйте и экспортируйте функцию emptyTagsCount(), которая считает количество пустых тегов. Тип тега задается первым параметром функции.
 */
 export function reduce(handler, accum, htmlList){
-    // функция должна
-    const fnRes = handler(element,accum)
-
+    // функция должна принять элемент и аккумулятор и выдать новый аккумулятор
+    if(isEmpty(htmlList)){
+        return accum;
+    }
+    accum = handler(head(htmlList),accum);
+    htmlList = tail(htmlList);
+    return reduce(handler, accum, htmlList);
 }
 
 const html1 = append(make(), node('h1', 'header1'));
 const html2 = append(html1, node('h1', 'header2'));
 const html3 = append(html2, node('p', 'content'));
 
-reduce((element, acc) => {
+const dom1 = make();
+const dom2 = append(dom1, node('h1', 'scheme'));
+const dom3 = append(dom2, node('p', 'is a lisp'));
+
+const dom4 = append(dom3, node('h1', 'haskell'));
+const dom5 = append(dom4, node('p', 'is a functional language'));
+
+const dom6 = append(dom5, node('h1', 'prolog'));
+
+const dom7 = append(dom6, node('h2', ''));
+const dom8 = append(dom7, node('span', ''));
+const dom = append(dom8, node('p', 'is about logic'));
+
+console.log(reduce((element, acc) => (is('h1', element) ? acc + 1 : acc), 0, dom)); //3
+
+console.log(reduce((element, acc) => {
     return is('h1', element) ? acc + 1 : acc;
-}, 0, html3); // 2
+}, 0, html3)); // 2
 
-export function emptyTagsCount(){
+console.log(reduce((element, acc) => {
+    const content = getValue(element);
+    return is('h1', element) ? `${acc} ${content}` : acc;
+}, 'Languages:', dom)); //'Languages: prolog haskell scheme'
 
+console.log(reduce((_element, acc) => acc + 1, 0, dom)); //8
+
+
+export function emptyTagsCount(tagName, htmlList){
+    let count = 0;
+    function summer(htmlList){
+        if(isEmpty(htmlList)){
+            return count;
+        }
+        let next = head(htmlList);
+        if(getName(next) === tagName && !getValue(next)){
+            count += 1;
+        }
+        htmlList = tail(htmlList);
+        return summer(htmlList);
+    }
+
+    return summer(htmlList);
 }
 
 const html4 = make();
@@ -30,4 +70,4 @@ const html7 = append(html6, node('blockquote', ''));
 const html8 = append(html7, node('blockquote', ''));
 const html9 = append(html8, node('blockquote', 'quote'));
 
-emptyTagsCount('blockquote', html9); // 2
+console.log(emptyTagsCount('blockquote', html9)); // 2
