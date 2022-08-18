44071e64abb04d989f4788394ef64f221ce9a5fc
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-08-18 17:41:50 +0300

    feat phonebook parsing and query params behaviour

--- js-http-server/2/index.js
+++ js-http-server/2/index.js
@@ -1,15 +1,35 @@
+/* Реализуйте логику парсинга файла phonebook.txt в следующий формат:
+    {
+      <id>: {
+        name: <name>,
+        phone: <phone>,
+      },
+      <id>: {
+        name: <name>,
+        phone: <phone>,
+      },
+    }
+Где <id> - это идентификатор конкретной записи, а <name> и <phone> – это имя и телефон соответствующей записи.*/
+
 import fs from 'fs/promises';
-import _ from 'lodash';
 
 import makeServer from './server.js';
 
-export default async (port, callback = () => {}) => {
+export default async function index(port, callback = () => {}){
     const data = await fs.readFile('phonebook.txt', 'utf-8');
 
-    // BEGIN (write your solution here)
+    // BEGIN (write your solution here);
+        const usersById = {};
 
+        const usersArray = data.split('\n');
+        usersArray.forEach(userString => {
+            const [id,name,phone] = userString.split(' | ');
+            usersById[id] = {name:name, phone:phone};
+        })
     // END
 
     const server = makeServer(usersById);
     server.listen(port, () => callback(server));
 };
+
+index(3333).then(() => console.log('index works'))
--- js-http-server/2/server.js
+++ js-http-server/2/server.js
@@ -1,4 +1,18 @@
+/* Сервер позволяет выполнять запросы на поиск всех записей, соответствующих критерию поиска.
+Критерием является часть имени/фамилии, по которой производится сопоставление.
+
+В запросе к серверу по ссылке /search необходимо передать один параметр: q, значением которого, будет подстрока.
+В случае, если найдены сопоставления, то сервер возвращает данные в следующем формате (для подстроки miss):
+
+    Miss Arlo Barrows, 328-949-3924
+    Miss Bernadette Conn, 249.059.5515
+    Miss Savannah Dicki, 157.463.3368
+    Miss Rudy Brown, 779-703-0150`
+Обратите внимание, что регистр при сопоставлении не учитывается.
+В случае если запрос к серверу выполняется без параметров или соответствий не найдено, он должен вернуть пустую строку.*/
+
 import http from 'http';
+import * as url from "url";
 
 export default (usersById) => http.createServer((request, response) => {
     request.on('end', () => {
@@ -8,9 +22,25 @@ export default (usersById) => http.createServer((request, response) => {
                 `Records count: ${Object.keys(usersById).length}`,
             ];
             response.end(messages.join('\n'));
-        } else if (request.url.startsWith('/search')) {
-            // BEGIN (write your solution here)
 
+        } else if (request.url.startsWith('/search')) {
+
+            // BEGIN (write your solution here)
+            const substr = url.parse(request.url,true).query.q?.toLowerCase();
+                console.log('substr = ',substr);
+            if(!substr){
+                response.end('');
+                return;
+            }
+            const users = Object.values(usersById);
+                console.log('users length = ', users.length);
+            const xz = users.reduce((acc,element) => {
+                if(element.name.toLowerCase().indexOf(substr) > -1){
+                    return acc + `${element.name}, ${element.phone}\n`
+                } else return acc;
+            },'')
+            const resp = xz.slice(0,-1)||'';
+            response.end(resp);
             // END
         }
     });
